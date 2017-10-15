f070d3eeb59f5fb2a6e76ab3b655bf97811ff664
IgnatievAnton <ignatievanton1996@gmail.com>
2017-10-15 13:10:07 +0300

    New build project version 4

--- script.bat
+++ script.bat
@@ -1,8 +1,18 @@
 @echo off
+color 0a
 title Script build Program
+set /P vers= < version.txt
+ECHO %vers%
+set /a vers+=1
+ECHO %vers%
+copy nul version.txt
+ECHO %vers% >>version.txt
 qmake -o Makefile main_widget_calendar.pro
+copy version.txt release/version.txt
 echo program build 	press "Enter" for create exe file.
-pause 
 make
 echo exe file create on path /relese.
+gitScript.bat
+echo New version push on git
 pause 
+%
