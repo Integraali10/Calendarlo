b4d5cb36a5c1f91e7d949f0bcc395cf23f1e34f1
IgnatievAnton <ignatievanton1996@gmail.com>
2017-10-16 21:10:38 +0300

    New build project version 10

---
+++ main_widget_calendar/script.bat
@@ -0,0 +1,22 @@
+@echo off
+color 0a
+title Script build Program
+set /P vers= < version.txt
+ECHO %vers%
+set /a vers+=1
+ECHO %vers%
+copy nul version.txt
+ECHO %vers% >>version.txt
+qmake -o Makefile main_widget_calendar.pro
+copy version.txt release/version.txt
+echo program build 	press "Enter" for create exe file.
+make
+echo exe file create on path /relese.
+d:
+cd qt\5.6\msvc2015_64\bin
+windeployqt.exe C:\Users\Anton\Desktop\calendar\Calendarlo\main_widget_calendar\release\main_widget_calendar.exe
+c:
+cd C:\Users\Anton\Desktop\calendar\Calendarlo\main_widget_calendar
+gitScript.bat
+echo New version push on git
+pause 
